a2974c998d1c9eee551f654d27a7e3d3767a9fce
Fhare <yago.yaguinho@live.com>
2023-05-23 14:15:44 -0300

    Adicionando configuração de CORS

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors({ origin: "0.0.0.0" }));
+app.use(express.json());
 
 app.post("/", (req, res) => {
   console.log("Bateu aqui na API");
